7103f128e3b73b8f3a87a61128ea917124fdf6f1
Tiarles Guterres <tiarlesmoralles@hotmail.com>
2020-05-13 17:10:37 -0300

    Clean of Reports

--- open_report.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-echo TyphoonTest pytest plugin.
-echo This window should remain opened for the report browser page to work.
-echo After you close the browser page you can close this window.
-echo ---
-typhoon-allure serve report
